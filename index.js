491224979698538f996d8500261e5959bd7741bd
jkpoea <jkpeoa@gmail.com>
2020-09-23 21:19:51 +0200

    Added the code for secretPass

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var port = 3000;
-var secretWord = 'lies'
+var secretPass = 'LIES'
 const fs = require('fs');
 const app = express();
 
@@ -10,13 +10,26 @@ app.get("/" ,(req, res) => {
         if (err) throw err;
         res.write(data)
         res.end();
-    })
+    });
 });
 
-app.get("/", (req, res) => {
-    let guess
-})
+app.get("/secret", (req, res) => {
+    let urlPass = req.query.key;
+
+    if(urlPass == secretPass){
+
+        fs.readFile('secret.html', (err, data) =>{
+            if (err) throw err;
+            res.write(data)
+            res.end();
+        });
+    }
+    else{
+        res.write('Error, wrong password. Better luck next time!')
+        res.end();
+    }
+});
 
 app.listen(port, () => {
-    console.log('Working as intended')
+    console.log('Working as intended at port ' + port)
 })
